d1da1cfa1e9198f4c5795f0cc4e387c6a2496a99
KzAshimo <good.ashimo@gmail.com>
2024-11-22 21:41:36 +0900

    delete修正

--- next.js-revenge/src/app/api/board/delete/route.ts
+++ next.js-revenge/src/app/api/board/delete/route.ts
@@ -1,39 +1,23 @@
 import { supabase } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const searchTerm = searchParams.get("searchTerm");
-  const startDate = searchParams.get("startDate");
-// boardテーブルから検索メソッド　フロント側で削除処理
+export async function DELETE(req: Request) {
   try {
-    let query = supabase.from('board').select('*');
+    const { id } = await req.json();
 
-    if (searchTerm) {
-      query = query.ilike('title', `%${searchTerm}%`).or(`ilike(content, %${searchTerm}%)`);
+    if (!id) {
+      return NextResponse.json({ message: "IDが提供されていません" }, { status: 400 });
     }
 
-    if (startDate) {
-      const parsedStartDate = new Date(startDate);
-      if (!isNaN(parsedStartDate.getTime())) {
-        query = query.gte('created_at', parsedStartDate.toISOString());
-      } else {
-        throw new Error("Invalid date format");
-      }
-    }
-
-    query = query.order('created_at', { ascending: false });
-
-    const { data: posts, error } = await query;
-
+    const { error } = await supabase.from("board").delete().eq("board_id", id);
     if (error) {
-      console.error('データベースエラー:', error.message);
-      return NextResponse.json({ message: "データベースエラー" }, { status: 500 });
+      console.error("データ削除エラー:", error.message);
+      return NextResponse.json({ message: "削除エラー" }, { status: 500 });
     }
 
-    return NextResponse.json({ posts });
+    return NextResponse.json({ message: "削除成功" }, { status: 200 });
   } catch (error) {
     console.error("予期しないエラー:", error);
-    return NextResponse.json({ message: "データベースエラー" }, { status: 500 });
+    return NextResponse.json({ message: "サーバーエラー" }, { status: 500 });
   }
 }
